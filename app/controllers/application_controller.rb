1fb96edce36d833b3e35eaa84d6b43bdcfdb1b26
Liam Simmons <liam.simmons@powerhrg.com>
2024-07-26 12:13:28 -0400

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,12 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
   get '/games' do
-    # get all the games from the database
     games = Game.all
-    # return a JSON response with an array of all the game data
     games.to_json
   end
 
   get '/games/:id' do
-    # look up the game in the database using its ID
     game = Game.find(params[:id])
-    # send a JSON-formatted response of the game data
     game.to_json(include: {reviews: {include: :user}})
   end
 end
